d22eb7fce03132ce43be59d31b4ded6b7a9995cc
Swagat Konchada <swagatata@gmail.com>
2019-10-02 15:00:24 -0700

    +linux_setup.sh +vimrc

---
+++ linux_setup.sh
@@ -0,0 +1,7 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+sudo yum groupinstall 'Development Tools'
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+cat brewlist | xargs brew install
